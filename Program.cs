8b540baf643778a03dca45250884718f0b57b2d4
kmerck88 <Kevin.Merck88@gmail.com>
2022-05-30 16:23:46 -0400

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace Deliverable1
+{
+	class Program
+	{
+		static void Main(string[] args)
+				
+        {
+		
+       string yn = "y";
+do
+{
+    Console.Write("How many people are we making PB&J sandwiches for? ");
+    int numbofPeople;
+    if (int.TryParse(Console.ReadLine(), out numbofPeople))
+    {
+        int hungryPeople = numbofPeople * 2;
+        Console.WriteLine("You need:");
+        Console.WriteLine($"{hungryPeople} slices of bread");
+        Console.WriteLine($"{hungryPeople} tablespoons of peanut butter");
+        Console.WriteLine($"{hungryPeople * 2} teaspoons of jelly");
+
+        Console.WriteLine("which is...");
+        Console.WriteLine("You need:");
+        int loavesBread = (int)(double)Math.Ceiling((double)hungryPeople / 28);
+        Console.WriteLine($"{loavesBread} loaves of bread");
+        int jarsPB = (int)(double)Math.Ceiling((double)hungryPeople / 32);
+        Console.WriteLine($"{jarsPB} jars of peanut butter");
+        int jarsJ = (int)(double)Math.Ceiling((double)hungryPeople * 2 / 48);
+        Console.WriteLine($"{jarsJ} jars of jelly");
+    }
+    else
+    {
+        Console.WriteLine("Sorry, that was not an integer.");
+    }
+    Console.Write("Would you like to restart? Enter yes or y to continue, or any other key to exit: ");
+    yn = Console.ReadLine();
+}
+while (yn == "y" || yn == "yes");
+Console.WriteLine("Goodbye!");
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,1 +1,21 @@
-"# Deliverable1" 
+# TODO List 
+
+- [x] Ask the user to input a number and store that number in a variable. Assume the user will type a valid number.
+
+- [] Total slices of bread is correctly calculated and printed to the console.
+
+- [] Total tablespoons of peanut butter is correctly calculated and printed to the console.
+
+- [] Total teaspoons of jelly is correctly calculated and printed to the console.
+
+- [] Total amount of loaves is correctly calculated and printed to console (even if not correctly rounded).
+
+- [] Total amount of peanut butter jars and jelly jars is correctly calculated and printed to console (even if not correctly rounded).
+
+- [] All three amounts of loaves and jars are correctly rounded up.
+
+- [] If the user responds ‘y’ to restart, loop back to the top of the program.
+
+- [] if the user responds ‘yes’ to restart, loop back to the top of the program.
+
+- [] If the user responds with anything else, print "Goodbye" and let the program end.
